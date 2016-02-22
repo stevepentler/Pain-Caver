e64d85cbd3d5af356386c0212c5725c7ec7c0e21
Steve Pentler <stevepentler@gmail.com>
2016-02-21 20:44:29 -0700

    Creates and migrates User model

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    if @user
+      session[:user_id] = @user.id
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
+  end
+end
