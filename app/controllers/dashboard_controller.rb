79b628f2715c75852c5f4cb0885affa961eddf80
Steve Pentler <stevepentler@gmail.com>
2016-02-27 00:01:17 -0700

    Adds individual workout data to strava_service

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,6 @@ class DashboardController <ApplicationController
   before_action :authorize!
   
   def show
-    @athlete = StravaService.new(current_user).retrieve_current_athlete
     @activities = StravaService.new(current_user)
   end
 end
--- app/services/strava_service.rb
+++ app/services/strava_service.rb
@@ -7,10 +7,6 @@ class StravaService
     @client = Strava::Api::V3::Client.new(:access_token => ENV['MY_ACCESS_TOKEN'])
   end
 
-  def retrieve_current_athlete
-    client.retrieve_current_athlete
-  end
-
   def list_athlete_activities
     client.list_athlete_activities
   end
@@ -19,8 +15,44 @@ class StravaService
     activity["total_elevation_gain"]
   end
 
-  # def total_elevation_gain
-  #   client.list_athlete_activities["total_elevation_gain"]
-  # end
+  def name(activity)
+    activity["name"]
+  end
+
+  def distance(activity)
+    activity["distance"]
+  end
+
+  def duration(activity)
+    activity["time"]
+  end
+
+  def elev_high(activity)
+    activity["elev_high"]
+  end
+
+  def elev_low(activity)
+    activity["elevation_low"]
+  end
+
+  def type(activity)
+    activity["type"]
+  end
+
+  def start_date(activity)
+    activity["start_date"]
+  end  
+
+  def average_speed(activity)
+    activity["average_speed"]
+  end  
+
+  def max_speed(activity)
+    activity["max_speed"]
+  end  
+
+  def calories(activity)
+    activity["calories"]
+  end
 
 end
