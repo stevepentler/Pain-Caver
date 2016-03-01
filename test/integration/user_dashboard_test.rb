730cd58a53fc3b6f5b166c7831c00bb94319552f
Steve Pentler <stevepentler@gmail.com>
2016-03-01 14:54:08 -0700

    User dashboard navbar test

--- test/integration/user_dashboard_test.rb
+++ test/integration/user_dashboard_test.rb
@@ -9,11 +9,35 @@ class UserDashboardTest < ActionDispatch::IntegrationTest
   end
 
   test 'user views monthly stats on dashboard' do
-      login
+    login
     VCR.use_cassette('stats') do
       visit dashboard_path
       assert_equal dashboard_path, current_path
+      
       assert page.has_content?(@user.first_name)
+      assert page.has_content?(@user.city)
+      assert page.has_content?(@user.state)
+      assert page.has_content?("Monthly Stats")
+      assert page.has_content?("sessions")
+      assert page.has_content?("miles")
+      assert page.has_content?("hr")
+      assert page.has_content?("mins")
+      assert page.has_content?("feet")
+    end
+  end
+
+  test 'user views dashboard navbar' do 
+    login
+    VCR.use_cassette('stats') do
+      visit dashboard_path
+      assert_equal dashboard_path, current_path
+
+      within("nav") do 
+        assert page.has_content?("PAINCAVER")
+        assert page.has_content?("View Workouts")
+        assert page.has_content?("Dashboard")
+        assert page.has_content?("Logout")
+      end
     end
   end
 
