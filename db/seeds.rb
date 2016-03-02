41c9561092a2a1e37b6e68c2431d18008bed7283
Steve Pentler <stevepentler@gmail.com>
2016-03-02 15:27:03 -0700

    Formats Course card on workout show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@ leadville = Race.create(title: "Leadville 100",
                         elevation_gain: 18168.0,
                         max_elevation: 12600.0,
                         distance: 100.0,
-                        description: "The legendary \"Race Across The Sky\" 100-mile run is where it all started back in 1983. This is it. The race where legends are created and limits are tested. One hundred miles of extreme Colorado Rockies terrain — from elevations of 9,200 to 12,600 feet. You will give the mountain respect, and earn respect from all",
+                        description: "The legendary \"Race Across The Sky\" 100-mile run is where it all started back in 1983. This is it. The race where legends are created and limits are tested. One hundred miles of extreme Colorado Rockies terrain — from elevations of 9,200 to 12,600 feet. You will give the mountain respect, and earn respect from all.",
                         website: "http://www.leadvilleraceseries.com/run/leadvilletrail100run/"
                         )
 
@@ -32,4 +32,13 @@ badwater = Race.create(title: "Badwater 135",
                        distance: 135.0,
                        description: "Covering 135 miles (217km) non-stop from Death Valley to Mt. Whitney, CA, the STYR Labs Badwater® 135 is the most demanding and extreme running race offered anywhere on the planet. The start line is at Badwater, Death Valley, which marks the lowest elevation in North America at 280’ (85m) below sea level. The race finishes at Whitney Portal at 8,300’ (2530m).",
                        website: "http://www.badwater.com/event/badwater-135/"
+                      )
+
+hardrock = Race.create(title: "Hardrock 100",
+                       title_id: "hardrock",
+                       elevation_gain: 33992.0,
+                       max_elevation: 14048.0,
+                       distance: 100.5,
+                       description: "The run starts and ends in Silverton, Colorado and travels through the towns of Telluride, Ouray, and the ghost town of Sherman, crossing thirteen major passes in the 12,000' to 13,000' range. Entrants must travel above 12,000 feet (3,700 m) of elevation a total of 13 times, with the highest point on the course being the 14,048' summit of Handies Peak.",
+                       website: "http://hardrock100.com/index.php"
                       )
