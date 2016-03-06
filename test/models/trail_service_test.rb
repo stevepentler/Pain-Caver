4c3538dd852f0eb2a9ffdce301bf7622487ab767
Steve Pentler <stevepentler@gmail.com>
2016-03-06 12:27:29 -0700

    Sets up trail service model test

--- test/models/trail_service_test.rb
+++ test/models/trail_service_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
-class TrailServiceTest
+class TrailServiceTest < ActiveSupport::TestCase
 
   test 'trail attributes' do 
     VCR.use_cassette('trails') do 
-      user = create(:user)
+      current_user = create(:user)
       service = TrailService.new(current_user)
     end
   end
