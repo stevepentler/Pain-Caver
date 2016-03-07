e2fc8ce457661c8a49c351b79bf0c18b252ad40d
Steve Pentler <stevepentler@gmail.com>
2016-03-07 14:00:26 -0700

    adds present for dashboard action and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,11 +2,12 @@ class DashboardController <ApplicationController
   before_action :authorize!
   
   def show
-    @stats = UserStatsService.new(current_user)
-    @upcoming_races = current_user.user_races.order(:date)
-    tip_count = rand(RunningTip.count)
-    @running_tip = RunningTip.offset(tip_count).first
-    @trail_service = TrailService.new(current_user)
+    @presenter = DashboardPresenter.new(current_user)
+    # @stats = UserStatsService.new(current_user)
+    # @upcoming_races = current_user.user_races.order(:date)
+    # tip_count = rand(RunningTip.count)
+    # @running_tip = RunningTip.offset(tip_count).first
+    # @trail_service = TrailService.new(current_user)
     # WorkoutIndexWorker.perform_async(current_user)
   end
 end
