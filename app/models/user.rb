c21cb8d6ee9538b1881911b43cfda0a0f1df7a1a
Steve Pentler <stevepentler@gmail.com>
2016-03-08 19:54:30 -0700

    Adds user recent id to pass workout index test, resets cassettes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ActiveRecord::Base
   def self.find_or_create_by_auth(auth)
     user = User.find_or_create_by(provider: auth[:provider], user_id: auth[:uid])
     user_attributes(user, auth)
-
     user.save
     return user
   end
