867450a3fbaadb33b4f022a2f7af77bdae45a4ea
Steve Pentler <stevepentler@gmail.com>
2016-03-09 12:18:43 -0700

    Adds methods to workouts index worker

--- app/workers/workout_index_worker.rb
+++ app/workers/workout_index_worker.rb
@@ -2,10 +2,11 @@ class WorkoutIndexWorker
   include Sidekiq::Worker 
   
   def perform(current_user)
-    @presenter = Presenter::WorkoutsPresenter.new(current_user)
-    # @presenter.service
+    @presenter = WorkoutsPresenter.new(current_user)
+    @presenter.service
     @presenter.activities
-    # @presenter.stats
-    # @presenter.running_tip
+    @presenter.stats
+    @presenter.running_tip
+    binding.pry
   end
 end
